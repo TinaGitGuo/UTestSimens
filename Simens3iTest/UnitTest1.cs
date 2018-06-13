644e69a32109638538094f4e53e0bfb4babf97be
郭易 <840322864@qq.com>
2018-06-13 14:51:06 +0800

    Add project files.

---
+++ Simens3iTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Simens3iTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
